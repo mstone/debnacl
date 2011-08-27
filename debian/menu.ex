c7e48a51b495adfd19733b48d6f437d3386361fa
Michael Stone <michael@laptop.org>
2011-08-27 14:00:53 -0400

    Remove example files and fix up debian/{copyright,control}.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nacl):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nacl" command="/usr/bin/nacl"
--- debian/nacl.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nacl package
-#
-0 4	* * *	root	[ -x /usr/bin/nacl_maintenance ] && /usr/bin/nacl_maintenance
